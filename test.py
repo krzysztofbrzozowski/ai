b12dfefceb25e3c62a0d9596c9a9f1b681085d3d
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2026-07-16 23:59:47 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,30 @@
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+import keras
+from keras import layers
+
+model = keras.Sequential(
+    [
+        layers.Dense(512, activation="relu"),
+        layers.Dense(10, activation="softmax"),
+    ]
+)
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"],
+)
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+if __name__ == "__main__":
+    # Evaluate the model
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print("Test accuracy:", test_acc)
